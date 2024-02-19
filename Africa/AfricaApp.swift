739c0ef4904618b4471a951164f9bdef3dacf703
shubhh123 <shubhh1181@gmail.com>
2024-02-19 23:29:07 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Shubham on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
