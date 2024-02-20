36e28993f1475c02fb7691486bc7cfbe3432078b
shubhh123 <shubhh1181@gmail.com>
2024-02-20 20:36:43 +0530

    Implemented Generics and Populated animal.json data in ContentView.

---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,8 @@
+//
+//  AnimalModel.swift
+//  Africa
+//
+//  Created by Shubham on 20/02/24.
+//
+
+import Foundation
---
+++ Africa/View/AnimalListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalListItemView.swift
+//  Africa
+//
+//  Created by Shubham on 20/02/24.
+//
+
+import SwiftUI
+
+struct AnimalListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalListItemView()
+}
